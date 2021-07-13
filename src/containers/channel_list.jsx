a1a57e70f72c5b42781f93f3815b505da83fff3c
D. Ip <ipkinghangdesmond@gmail.com>
2021-07-13 12:12:23 +0800

    made a simple redux post and get chat forum for wagonchat

--- src/containers/channel_list.jsx
+++ src/containers/channel_list.jsx
@@ -1,14 +1,55 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { selectChannel, fetchMessages } from '../actions/index';
+
 
 class ChannelList extends Component {
+
+  componentWillReceiveProps = (nextProps) => {
+    if (nextProps.selectedChannel !== this.props.selectedChannel) {
+      this.props.fetchMessages(nextProps.selectedChannel);
+    }
+  }
+
+  handleClick = (channel) => {
+    this.props.selectChannel(channel);
+  }
+
+  renderChannel = (channel) => {
+    return (
+      <li
+        key={channel}
+        className={channel === this.props.selectedChannel ? 'active' : null}
+        role="presentation"
+        onClick={() => this.handleClick(channel)}
+      >
+        #{channel}
+      </li>
+    );
+  }
 
   render() {
     return (
       <div className="col-sm-4">
-        <h1>Hello</h1>
+        <h1>Channel list</h1>
+        <ul>
+          {this.props.channels.map(this.renderChannel)}
+        </ul>
       </div>
     );
   }
 }
 
-export default ChannelList;
+function mapStateToProps(state) {
+  return {
+    channels: state.channels,
+    selectedChannel: state.selectedChannel
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ selectChannel, fetchMessages }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ChannelList);
